e536a18e16ba4e2e6ff0f281054b3adf474b3452
anasmersal <anasmersal@gmail.com>
2024-01-06 15:35:32 +0200

    Week 2

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,8 @@ from .views import *
 urlpatterns = [
     path("home/", home, name="home"),
     path("", index),
+    path("booking/", bookingview.as_view()),
+    path("menu/", menuview.as_view()),
+    path("menu/", MenuItemsView.as_view()),
+    path("menu/<int:pk>", SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from .serializers import bookingSerializer, menuSerializer
+from .models import Menu, Booking
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
 
 
 def home(request):
@@ -10,3 +13,39 @@ def home(request):
 
 def index(request):
     return render(request, "index.html", {})
+
+
+class bookingview(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = bookingSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = bookingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+
+class menuview(APIView):
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = menuSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = menuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
