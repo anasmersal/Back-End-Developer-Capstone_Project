c8e1d325d546f7ef27209b4f695a8e869d75911c
anasmersal <anasmersal@gmail.com>
2024-01-07 13:38:25 +0200

    add readme

---
+++ littlelemon/Readme.txt
@@ -0,0 +1,5 @@
+http://127.0.0.1:8000/restaurant/
+http://127.0.0.1:8000/restaurant/menu/items/
+http://127.0.0.1:8000/restaurant/menu/items/<int:pk>/
+http://127.0.0.1:8000/restaurant/api-token-auth/
+http://127.0.0.1:8000/admin/
--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Menu, Booking
-from django.contrib.auth.models import User
 
 
 class menuSerializer(serializers.ModelSerializer):
@@ -13,9 +12,3 @@ class bookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "groups"]
